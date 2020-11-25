b58f76225efd47b00582ffa8c40662d03c855e50
Sky <sky@sky.camp>
2020-11-25 10:54:00 -0600

    react context cleanup

--- react-context-blog/src/components/Blog.js
+++ react-context-blog/src/components/Blog.js
@@ -1,14 +1,12 @@
-import Posts from './Posts';
-import Post from './Post';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { BlogContextProvider } from '../context/blogContext';
+import Posts from './Posts';
+import Post from './Post';
 
 const Blog = (props) => {
     const { match } = props;
     const path = match.path;
-
-    // useEffect(getPosts, [getPosts]);
 
     return (
         <BlogContextProvider>
